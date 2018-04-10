cc939d0bdde14e861c5978cca9ae8743651e0579
D7703_03 <d7703_03@D7703-03ui-iMac.local>
2018-04-10 15:15:23 +0900

    add UISwitch

--- Digital Timer/ViewController.swift
+++ Digital Timer/ViewController.swift
@@ -10,16 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var timerSwitch: UISwitch!
+    
+    var myTimer = Timer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+      timerSwitch.setOn(false, animated: true)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @objc func updateTime() {
+        let date = Date()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "HH:mm:ss EE"
+        timeLabel.text = formatter.string(from: date)
     }
-
-
+  
+    @IBAction func timerStart(_ sender: Any) {
+        if timerSwitch.isOn == true {
+         //  myTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
+            
+            myTimer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: {(myTimer) in self.updateTime()
+            })
+        } else {
+            myTimer.invalidate()
+        }
+    }
+    
 }
 
